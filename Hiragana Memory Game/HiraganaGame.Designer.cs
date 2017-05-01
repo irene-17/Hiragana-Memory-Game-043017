8906af18e561c54eed43e2a5d82afb1d57cd5330
Brandon <bkmiller0412@gmail.com>
2017-05-01 13:46:42 -0600

    made rich text box / form just a tiny bit wider to fix overflow problem with text

--- Hiragana Memory Game/HiraganaGame.Designer.cs
+++ Hiragana Memory Game/HiraganaGame.Designer.cs
@@ -3568,7 +3568,7 @@
             this.RichTextBox_HowToPlay.Location = new System.Drawing.Point(14, 100);
             this.RichTextBox_HowToPlay.Name = "RichTextBox_HowToPlay";
             this.RichTextBox_HowToPlay.ReadOnly = true;
-            this.RichTextBox_HowToPlay.Size = new System.Drawing.Size(401, 64);
+            this.RichTextBox_HowToPlay.Size = new System.Drawing.Size(417, 64);
             this.RichTextBox_HowToPlay.TabIndex = 64;
             this.RichTextBox_HowToPlay.Text = "";
             // 
@@ -3630,7 +3630,7 @@
             this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(437, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(443, 24);
             this.menuStrip1.TabIndex = 71;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -3677,7 +3677,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 463);
+            this.ClientSize = new System.Drawing.Size(443, 463);
             this.Controls.Add(this.howToPlayButton);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.Label_ResultMessage);
