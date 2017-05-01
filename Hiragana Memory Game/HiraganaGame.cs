dc97dfc5793847bc960427cc3ec9ba13fbc68557
Patrick Condon <csneon2000@gmail.com>
2017-05-01 13:50:40 -0600

    Removed line 76, was for testing to see if i was synced to github correctly

--- Hiragana Memory Game/HiraganaGame.cs
+++ Hiragana Memory Game/HiraganaGame.cs
@@ -73,7 +73,6 @@ namespace Hiragana_Memory_Game
 
             InitializeComponent();
             //The following was written by Shawn Murrish:
-            //Testing Please remove
             this.aTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
             this.iTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
             this.uTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
